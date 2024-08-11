a2d2eb39e482368ab6750978f78a53727321d483
George <30464774+Getos@users.noreply.github.com>
2024-08-11 17:02:42 +0300

    added user app with login/out/register functionality

---
+++ ecommerce/users/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import tokens
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('home')
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            print(tokens)
+            login(request, form.get_user())
+            return redirect(reverse('home'))  # Redirect to the home page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
